9ff6d167dad98b2066754cc727496fbe6897a39b
Mike Griffin <mgriffin@github.com>
2024-02-29 11:52:53 +0000

    Make it into an array, sheesh

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "grff_in", roles: %[app db web]
+server "grff_in", roles: %w[app db web]
 
 # role-based syntax
 # ==================
